90f94360aea56db0d6ae9f24209de024d91d16b2
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-05-24 11:42:56 +0700

    Add files via upload

---
+++ bai17.php
@@ -0,0 +1,7 @@
+<?php
+//17Viết một chương trình PHP để kiểm tra xem một chuỗi có chứa một chuỗi con khác không sử dụng hàm strstr().
+function findString17($string, $string1){
+    return strpos($string, $string2);
+}
+echo findString17("dinh thanh", "thanh");
+?>
