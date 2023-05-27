082f1e3b09711ffddb8c8691beb93b85738aeeaa
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-05-28 00:50:23 +0700

    Update bai17.php

--- bai17.php
+++ bai17.php
@@ -1,7 +1,10 @@
 <?php
 //17Viết một chương trình PHP để kiểm tra xem một chuỗi có chứa một chuỗi con khác không sử dụng hàm strstr().
-function findString17($string, $string1){
-    return strpos($string, $string2);
-}
-echo findString17("dinh thanh", "thanh");
+$findString17 = strpos("dinh thanh", "thanh");
+   if ($findString17 !== false) {
+   echo "chuoi co chua chuoi con ";
+   } else {
+             echo "chuoi khong chua chuoi con";
+   }
+
 ?>
