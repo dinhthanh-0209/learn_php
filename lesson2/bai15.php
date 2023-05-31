3dcb1117f2e4f200ed9d4d9cb0e5b1fbf687a745
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-05-31 19:55:09 +0700

    Create bai15.php

---
+++ lesson2/bai15.php
@@ -0,0 +1,7 @@
+<?php
+//15Viết một chương trình PHP để thêm một chuỗi vào đầu hoặc cuối của một chuỗi sử dụng hàm str_pad().
+function addString($string, $string1, $string2){
+    $result = addString($string, $string1, $string2);
+}
+echo addString("Dinh","Thanh","<3");
+?>
