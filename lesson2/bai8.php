670c0f73b3d8a077f1c5345ac30d0bfdebf4a8c3
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-05-31 19:55:56 +0700

    Create bai8.php

---
+++ lesson2/bai8.php
@@ -0,0 +1,7 @@
+<?php
+//8Viết một chương trình PHP để chuyển đổi một chuỗi thành chữ thường sử dụng hàm strtolower().
+function changeToLower($string){
+    return strtolower($string);
+}
+echo changeToLower("THANH");
+?>
