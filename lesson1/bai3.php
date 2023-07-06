1cd0019284c6ee180cd7affd60a2f47910783baa
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-06 09:19:14 +0700

    Create bai3.php

---
+++ lesson1/bai3.php
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+<?php //3Viết một chương trình PHP để đảo ngược một chuỗi sử dụng hàm strrev().
+function revertString($string){
+    return strrev($string);
+}
+echo revertString("Thanh");
+=======
+<?php //3Viết một chương trình PHP để đảo ngược một chuỗi sử dụng hàm strrev().
+function revertString($string){
+    return strrev($string);
+}
+echo revertString("Thanh");
+>>>>>>> b98672a78b33bd58ec13a3ccb235304b7a0706c8
+?>
