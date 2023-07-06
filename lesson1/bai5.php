436590cf23638d0c2bd9180615be05efd1e25310
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-06 09:19:21 +0700

    Create bai5.php

---
+++ lesson1/bai5.php
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+<?php
+//5Viết một chương trình PHP để thay thế một chuỗi con trong một chuỗi bằng một chuỗi khác sử dụng hàm str_replace().
+function replaceString($string1,$string2,$string){
+    return str_replace($string1,$string2,$string);
+}
+echo replaceString("Thanhh","Thanh","Dinh Thanh");
+=======
+<?php
+//5Viết một chương trình PHP để thay thế một chuỗi con trong một chuỗi bằng một chuỗi khác sử dụng hàm str_replace().
+function replaceString($string1,$string2,$string){
+    return str_replace($string1,$string2,$string);
+}
+echo replaceString("Thanhh","Thanh","Dinh Thanh");
+>>>>>>> b98672a78b33bd58ec13a3ccb235304b7a0706c8
+?>
