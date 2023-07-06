9473f3623d4881e1d2ca1cc5f9606e0a004c8d1d
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-06 09:19:07 +0700

    Create bai2.php

---
+++ lesson1/bai2.php
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+<?php //2Viết một chương trình PHP để đếm số từ trong một chuỗi sử dụng hàm str_word_count().
+function countWord($string){
+    return str_word_count($string);
+    echo countWord("Thanh");
+}
+=======
+<?php //2Viết một chương trình PHP để đếm số từ trong một chuỗi sử dụng hàm str_word_count().
+function countWord($string){
+    return str_word_count($string);
+    echo countWord("Thanh");
+}
+>>>>>>> b98672a78b33bd58ec13a3ccb235304b7a0706c8
+?>
