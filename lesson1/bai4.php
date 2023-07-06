fe8c4ec855c9bb158d85b22f3b6b82c91c349981
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-06 09:19:17 +0700

    Create bai4.php

---
+++ lesson1/bai4.php
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+<?php
+//4Viết một chương trình PHP để tìm kiếm một chuỗi con trong một chuỗi sử dụng hàm strpos().
+function searchString($string1,$string){
+    return strpos($string1,$string);
+}
+echo searchString("Thanh","Dinh Thanh");
+?>
+=======
+<?php
+//4Viết một chương trình PHP để tìm kiếm một chuỗi con trong một chuỗi sử dụng hàm strpos().
+function searchString($string1,$string){
+    return strpos($string1,$string);
+}
+echo searchString("Dinh Thanh","Thanh");
+?>
+>>>>>>> b98672a78b33bd58ec13a3ccb235304b7a0706c8
