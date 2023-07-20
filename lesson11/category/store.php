cf90a76c88f0a9111884f8d51878645d2752dbe5
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-20 12:09:22 +0700

    Delete store.php

--- lesson11/category/store.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-    require_once 'pdopro.php';
-    $pro1 = new ProductQuery();
-    $data = ['name' => $_POST['name']];
-    $pro1->createNewData($data);
-    header("Location: index.php");
-?>
