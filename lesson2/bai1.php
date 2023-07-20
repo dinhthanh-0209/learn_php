6a1da7aabe66916f87956e9590b6737f350df1cc
dinhthanh-0209 <133091131+dinhthanh-0209@users.noreply.github.com>
2023-07-20 12:09:41 +0700

    Delete bai1.php

--- lesson2/bai1.php
+++
@@ -1,6 +0,0 @@
-<?php //1Viết một chương trình PHP để đếm số ký tự trong một chuỗi sử dụng hàm strlen().
-function countLength($string){
-    return strlen($string);
-}
-    echo countLength("Thanh");
-?> 
